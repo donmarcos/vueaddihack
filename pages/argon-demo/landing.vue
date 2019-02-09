62ce6107930299761844b7019e74b91333fa8e04
unknown <marcos.alois.oliva@gmail.com>
2019-02-09 14:06:02 -0800

    first commit

---
+++ pages/argon-demo/landing.vue
@@ -0,0 +1,119 @@
+<template>
+  <div>
+    <div class="position-relative">
+      <!-- shape Hero -->
+      <video-bg :sources="['/argon/img/theme/back.mp4']" img>
+        <section class="section-shaped my-0">
+          <div class="shape shape-style-1 shape-default shape-skew">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div class="container shape-container d-flex">
+            <div class="col px-0">
+              <div class="row">
+                <div class="col-lg-6">
+                  <h1 class="display-3 text-white">
+                    ACT Long Beach
+                    <span></span>
+                  </h1>
+                  <p class="lead text-white">Take action and make your elected official accountable</p>
+                  <div class="btn-wrapper">
+                    <base-button
+                      tag="a"
+                      href="https://demos.creative-tim.com/argon-design-system/docs/components/alerts.html"
+                      class="mb-3 mb-sm-0"
+                      type="info"
+                      icon="fa fa-code"
+                    >Join Us</base-button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </video-bg>
+
+      <!-- 1st Hero Variation -->
+    </div>
+    <section class="section section-lg pt-lg-0 mt--200">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-12">
+            <div class="row row-grid">
+              <div class="col-lg-4">
+                <card class="border-0" hover shadow body-classes="py-5">
+                  <icon name="ni ni-check-bold" type="primary" rounded class="mb-4"></icon>
+                  <h6 class="text-primary text-uppercase">Districts</h6>
+                  <p class="description mt-3">
+                    Long Beach has several districts
+                    where are you located ?
+                    Find out
+                  </p>
+                  <div>
+                    <badge type="primary" rounded>Location</badge>
+                    <badge type="primary" rounded>Residents</badge>
+                    <badge type="primary" rounded>Boundaries</badge>
+                  </div>
+                  <nuxt-link :to="{name: 'argon-demo-lbdisctricts'}">
+                    <base-button type="primary" class="mt-4">Discticts</base-button>
+                  </nuxt-link>
+                </card>
+              </div>
+              <div class="col-lg-4">
+                <card class="border-0" hover shadow body-classes="py-5">
+                  <icon name="ni ni-istanbul" type="success" rounded class="mb-4"></icon>
+                  <h6 class="text-success text-uppercase">Elected Officials</h6>
+                  <p class="description mt-3">
+                    Want to know who is your elected representative in Long Beach ?
+                    Let's find out
+                  </p>
+                  <div>
+                    <badge type="success" rounded>Goverment</badge>
+                    <badge type="success" rounded>vision</badge>
+                    <badge type="success" rounded>info</badge>
+                  </div>
+                  <nuxt-link :to="{name: 'argon-demo-lbelected'}">
+                    <base-button type="success" class="mt-4">Learn more</base-button>
+                  </nuxt-link>
+                </card>
+              </div>
+              <div class="col-lg-4">
+                <card class="border-0" hover shadow body-classes="py-5">
+                  <icon name="ni ni-planet" type="warning" rounded class="mb-4"></icon>
+                  <h6 class="text-warning text-uppercase">Resources</h6>
+                  <p class="description mt-3">
+                    Long Beach has multiple resources for its residents
+                    check the interactive map
+                  </p>
+                  <div>
+                    <badge type="warning" rounded>marketing</badge>
+                    <badge type="warning" rounded>product</badge>
+                    <badge type="warning" rounded>Business</badge>
+                  </div>
+                  <nuxt-link :to="{name: 'argon-demo-lbresources'}">
+                    <base-button type="warning" class="mt-4">Learn more</base-button>
+                  </nuxt-link>
+                </card>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  layout: 'argon-demo',
+  components: {}
+}
+</script>
---
+++ pages/argon-demo/lbresources.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="profile-page">
+    <section class="section-profile-cover section-shaped my-0">
+      <div class="shape shape-style-1 shape-primary shape-skew alpha-4">
+        <span></span>
+        <span></span>
+      </div>
+    </section>
+    <section class="section section-skew">
+      <div class="container">
+        <card shadow class="card-body mt--400" no-body>
+          <style>
+            .embed-container {
+              position: relative;
+              padding-bottom: 80%;
+              height: 0;
+              max-width: 100%;
+            }
+            .embed-container iframe,
+            .embed-container object,
+            .embed-container iframe {
+              position: absolute;
+              top: 0;
+              left: 0;
+              width: 100%;
+              height: 100%;
+            }
+            small {
+              position: absolute;
+              z-index: 40;
+              bottom: 0;
+              margin-bottom: -15px;
+            }
+          </style>
+          <div class="embed-container">
+            <small>
+              <a
+                href="//www.arcgis.com/apps/Embed/index.html?webmap=2ff8e7f59c9b470f8397eeeed0fda045&extent=-118.3418,33.7248,-117.9566,33.872&home=true&zoom=true&scale=true&search=true&searchextent=true&details=true&legendlayers=true&active_panel=legend&basemap_gallery=true&disable_scroll=false&theme=light"
+                style="color:#0000FF;text-align:left"
+                target="_blank"
+              >View larger map</a>
+            </small>
+            <br>
+            <iframe
+              width="500"
+              height="400"
+              frameborder="0"
+              scrolling="no"
+              marginheight="0"
+              marginwidth="0"
+              title="MapIt Service Locations (Public Web Map)"
+              src="//www.arcgis.com/apps/Embed/index.html?webmap=2ff8e7f59c9b470f8397eeeed0fda045&extent=-118.3418,33.7248,-117.9566,33.872&home=true&zoom=true&previewImage=false&scale=true&search=true&searchextent=true&details=true&legendlayers=true&active_panel=legend&basemap_gallery=true&disable_scroll=false&theme=light"
+            ></iframe>
+          </div>
+
+          <div class="px-4">
+            <!--  
+            <div class="row justify-content-center">
+              <div class="col-lg-3 order-lg-2">
+                <div class="card-profile-image">
+                  <a href="#">
+                    <img v-lazy="'/argon/img/theme/team-4-800x800.jpg'" class="rounded-circle">
+                  </a>
+                </div>
+              </div>
+              
+              <div class="col-lg-4 order-lg-3 text-lg-right align-self-lg-center">
+                <div class="card-profile-actions py-4 mt-lg-0">
+                  <base-button type="info" size="sm" class="mr-4">Connect</base-button>
+                  <base-button type="default" size="sm" class="float-right">Message</base-button>
+                </div>
+              </div>
+              <div class="col-lg-4 order-lg-1">
+                <div class="card-profile-stats d-flex justify-content-center">
+                  <div>
+                    <span class="heading">22</span>
+                    <span class="description">Friends</span>
+                  </div>
+                  <div>
+                    <span class="heading">10</span>
+                    <span class="description">Photos</span>
+                  </div>
+                  <div>
+                    <span class="heading">89</span>
+                    <span class="description">Comments</span>
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+            
+            <div class="text-center mt-5">
+             
+              <h3>
+                Jessica Jones
+                <span class="font-weight-light">, 27</span>
+              </h3>
+              <div class="h6 font-weight-300">
+                <i class="ni location_pin mr-2"></i>Bucharest, Romania
+              </div>
+              <div class="h6 mt-4">
+                <i class="ni business_briefcase-24 mr-2"></i>Solution Manager - Creative Tim Officer
+              </div>
+              <div>
+                <i class="ni education_hat mr-2"></i>University of Computer Science
+              </div>
+            </div>
+            -->
+            <div class="mt-5 py-5 border-top text-center">
+              <div class="row justify-content-center">
+                <div class="col-lg-9"></div>
+              </div>
+            </div>
+          </div>
+        </card>
+      </div>
+    </section>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'argon-demo'
+}
+</script>
+<style>
+</style>
